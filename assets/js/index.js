f5c8fd41b1d4ac40ed47eabd1af43530a00a7f75
Abhiraj Chaudhuri <abhirajchaudhuri@gmail.com>
2023-04-19 18:40:38 +0530

    Updating Index JS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+// drop down menu
 const toggleBtn = document.querySelector('.toggle-btn')
 const toggleBtnIcon = document.querySelector('.toggle-btn i')
 const dropDownMenu = document.querySelector('.dropdown_menu')
@@ -9,4 +10,35 @@ toggleBtn.onclick = function () {
     toggleBtnIcon.classList = isOpen
         ? 'fa-solid fa-xmark'
         : 'fa-solid fa-bars'
+}
+
+
+// menu fever hacker animation
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+document.querySelector("h1").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 30);
 }
